421be93998432ce46ba98d319f8f9f498b2719c4
candicevivien <candicevivien@gmail.com>
2021-08-12 19:36:39 +1000

    stateless form component

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,36 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <label>
+      Name:
+      <input 
+        type = "text" 
+        value = {name} 
+        placeholder = "Name"
+        onChange = {({target}) => {setName(target.value)}}
+      />
+      </label>
+      <label>
+      Phone:
+      <input 
+        type="tel" 
+        value={phone} 
+        placeholder="+614XX-XXX-XXX"
+        pattern = "^(\+[6][1]*(\[0-9]*\|-[0-9]*-))?[0]?[1-9][0-9\- ]*$"
+        onChange = {({target}) => {setPhone(target.value)}}
+      />
+      </label>
+      <label>
+      Email:
+      <input 
+        type="email" 
+        value={email} 
+        placeholder="Email"
+        onChange = {({target}) => {setEmail(target.value)}}
+      />
+      </label>
+      <input type="submit" />
+    </form>
   );
 };
